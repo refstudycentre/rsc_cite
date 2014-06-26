86ad3055d5c3d62eee7306dabc1f62ef8adc101d
Rudolf Byker <git@1324966.no-reply.drupal.org>
2014-06-26 11:06:36 +0200

    simpler approach using field_view_field

--- rsc-cite-block.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $type = (empty($node->type) ? "" : $node->type);
-  $author = l($author->name,"taxonomy/term/{$author->tid}");
-  $citation = render($variables['citation']);
-  
-  print "<$tag class='$type citation'>$citation</$tag>";
-  print "<$tag class='$type author'>$author</$tag>";
-?>
